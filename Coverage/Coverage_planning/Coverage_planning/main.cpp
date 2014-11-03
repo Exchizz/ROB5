5be4e968ecddb0150d46a2e96ee80310faa2d842
Keerthikan Ratnarajah <kerat12@student.sdu.dk>
2014-11-03 11:29:34 +0100

    Ingen commit

---
+++ Coverage/Coverage_planning/Coverage_planning/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Coverage_planning
+//
+//  Created by Keerthikan Ratnarajah on 03/11/14.
+//  Copyright (c) 2014 Keerthikan Ratnarajah. All rights reserved.
+//
+
+#include <iostream>
+#include "Filehandle.h"
+int main(int argc, const char * argv[]) {
+    
+    Filehandle map("vej");
+    
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
