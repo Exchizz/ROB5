f591520f5e8c87f69904247c9f92f724c37d0691
nlynn12 <nicolai_lynnerup@hotmail.com>
2014-11-27 11:21:42 +0100

    Upload project

---
+++ src/planning/image.h
@@ -0,0 +1,64 @@
+/*
+ *	University of Southern Denmark
+ *	Robotics in context
+ *
+ *	Project:		ROB5 - Cup Collector
+ *	Module name:	image.h
+ *
+ *	Description:	Holds image class declaration
+ *
+ *	------------------------------------------------------
+ *	Change Log:
+ *
+ *	Date		ID		Change
+ *	YYMMDD
+ *
+ *	141107		MMN		Module Created
+ *
+ *	------------------------------------------------------
+ */
+
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+/***************************** Include files *******************************/
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <sstream>
+#include <algorithm> // std::reverse
+
+/*****************************    Defines    *******************************/
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Functions   *******************************/
+
+
+class Image {
+public:
+	Image();
+	int getWidth();
+	int getHeight();
+	int getMaxValue();
+	std::string getVersion();
+	int getPixel(int width, int height);
+	void setPixel(const int width, const int height, const int value);
+	void createVector(std::vector<std::vector<int>> &image);
+	void loadImage(const std::string filename);
+	void saveImage(std::string filename);
+private:
+	std::fstream imageFile;
+	std::vector<std::vector<int>> imageData;
+	int width;
+	int height;
+	int maxValue;
+	std::string inputLine;
+	std::string version;
+	std::stringstream ss;
+};
+
+
+#endif /* IMAGE_H_ */
---
+++ src/planning/main.cpp
@@ -0,0 +1,46 @@
+/*
+ *	University of Southern Denmark
+ *	Robotics in context
+ *
+ *	Project:		ROB5 - Cup Collector
+ *	Module name:	main.cpp
+ *
+ *	Description:
+ *
+ *	------------------------------------------------------
+ *	Change Log:
+ *
+ *	Date		ID		Change
+ *	YYMMDD
+ *
+ *	141123		NAL		Module Created
+ *						Updated for multiple OS
+ *
+ *	------------------------------------------------------
+ */
+
+/***************************** Include files *******************************/
+#include <iostream>
+#include <vector>
+#include "image.h"
+#include "planning.h"
+
+/*****************************    Defines    *******************************/
+#ifdef _WIN32
+#define windows 1;
+#else
+#define windows 0;
+#endif
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Functions   *******************************/
+
+int main() {
+	Planning img;
+	img.loadImage("test2.pgm");
+	auto listCenter = img.detect_rooms();
+	img.detect_center(listCenter);
+	img.saveImage("output.pgm");
+	return 0;
+}
